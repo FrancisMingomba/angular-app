1c27ec787155b05b9a0683f0544f8d5d2fa3db49
francis <mingomba@gmail.com>
2025-08-18 02:54:00 +0000

    Update component one

--- src/app/usernamepasswordreset/usernamepasswordreset.ts
+++ src/app/usernamepasswordreset/usernamepasswordreset.ts
@@ -1,13 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormGroup, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-usernamepasswordreset',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './usernamepasswordreset.html',
   styleUrl: './usernamepasswordreset.css'
 })
 export class Usernamepasswordreset {
+
 
 }
